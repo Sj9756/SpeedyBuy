f08113f87f3fc0b39b998827c73288b29e7f34bb
Sj9756 <sanjoshi8439@gmail.com>
2024-05-05 15:33:26 +0530

    fire base push custom noti. in background and forground both

--- app/src/main/java/com/example/speedybuy/firstActivity.java
+++ app/src/main/java/com/example/speedybuy/firstActivity.java
@@ -1,7 +1,11 @@
 package com.example.speedybuy;
 
 import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationChannelGroup;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,71 +13,105 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.speedybuy.AuthenticationU.Login;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessaging;
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
 
 public class firstActivity extends AppCompatActivity {
-Button start;
+    private static final String CHANNEL_ID = "Event";
+    private static final int MY_PERMISSIONS_REQUEST_POST_NOTIFICATIONS = 12;
+    Button start;
+
     @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_first);
-        start=findViewById(R.id.start);
+        start = findViewById(R.id.start);
+        setPermissions(Manifest.permission.POST_NOTIFICATIONS);
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if (!task.isSuccessful()) {
+                    Log.w("failed", "Fetching FCM registration token failed", task.getException());
+                    return;
+                }
+                String token = task.getResult();
+                Log.d("token", token);
+            }
+        });
+        start.setOnClickListener(this::openSecondActivity);
+        Intent intent=getIntent();
+        if(intent.hasExtra("action1")){
+            Log.d("action1","helo");
+            String taitle=intent.getStringExtra("action1");
+            String tait2e=intent.getStringExtra("action2");
+            Log.d("action1",taitle+" "+tait2e);
+        }
 
-        FirebaseMessaging.getInstance().getToken()
-                .addOnCompleteListener(new OnCompleteListener<String>() {
-                    @Override
-                    public void onComplete(@NonNull Task<String> task) {
-                        if (!task.isSuccessful()) {
-                            Log.w("failed", "Fetching FCM registration token failed", task.getException());
-                            return;
-                        }
-                        String token = task.getResult();
-                        Log.d("token",token);
-                    }
-                });
 
-
-        Dexter.withContext(this)
-                .withPermission(Manifest.permission.POST_NOTIFICATIONS)
-                .withListener(new PermissionListener() {
-                    @Override
-                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-
-                    }
-
-                    @Override
-                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-
-                    }
-
-                    @Override
-                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-
-                    }
-                })
-                .check();
-        start.setOnClickListener(this::OpenSecondActivity);
     }
-    public void OpenSecondActivity(View view)
-    {
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_PERMISSIONS_REQUEST_POST_NOTIFICATIONS) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                createNotificationChannel();
+            }
+        }
+    }
+    public void openSecondActivity(View view) {
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
-        overridePendingTransition(R.anim.slide_in_right,
-                R.anim.slide_out_left);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
+    private void setPermissions(@NonNull String permissions) {
+        ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                createNotificationChannel();
+            } else {
+
+            }
+
+        });
+
+        if (ContextCompat.checkSelfPermission(
+                this, permissions) ==
+                PackageManager.PERMISSION_GRANTED) {
+
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this, permissions)) {
+
+        } else {
+            requestPermissions(
+                    new String[]{Manifest.permission.POST_NOTIFICATIONS}, MY_PERMISSIONS_REQUEST_POST_NOTIFICATIONS);
+        }
+    }
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            NotificationChannelGroup group = new NotificationChannelGroup("44", "Event");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannelGroup(group);
+            channel.setGroup("44");
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
